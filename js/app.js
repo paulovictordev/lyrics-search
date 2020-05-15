8114c19d84c69a247312b2388690a7129bc11016
Paulo Victor <dev.paulovictor@gmail.com>
2020-05-15 09:18:54 -0300

    criando botoes de paginacao e buscando mais musicas

--- js/app.js
+++ js/app.js
@@ -3,7 +3,14 @@ const searchInput = document.querySelector('#search');
 const songsContainer = document.querySelector('#songs-container');
 const prevAndNextContainer = document.querySelector('#prev-and-next-container');
 
-const apiURL = `https://api.lyrics.ovh`
+const apiURL = `https://api.lyrics.ovh`;
+
+const getMoreSongs = async (url) => {
+  const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await response.json();
+
+  insertSongsIntoPage(data);
+}
 
 const insertSongsIntoPage = songsInfo => {
   songsContainer.innerHTML = songsInfo.data.map(song => `
@@ -12,6 +19,15 @@ const insertSongsIntoPage = songsInfo => {
       <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
     </li>
   `).join('');
+
+  if (songsInfo.prev || songsInfo.next) {
+    prevAndNextContainer.innerHTML = `
+      ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
+      ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
+    `;
+
+    return;
+  }
 }
 
 const fecthSongs = async (term) => {
